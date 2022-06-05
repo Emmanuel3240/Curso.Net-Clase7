06e1e7a6ea4d57978d659dc07709820ebeb90fc3
Emmanuel <emmanwel@gmail.com>
2022-06-05 13:10:56 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+var contador = 0;
+var acumulador = 0;
+var numero=0;
+var mayor = 0;
+var menor = 0;
+var promedio = 0;
+Console.WriteLine("A continuación ingrese 10 números");
+do
+{
+    contador++;
+    Console.WriteLine($"Ingrese el {contador}° número y presione ENTER: ");
+    numero = int.Parse(Console.ReadLine());
+    acumulador = acumulador + numero;
+    promedio = acumulador / contador;
+    //Obtener numero mayor y menor
+    if (contador == 1)
+    {
+        mayor = numero;
+        menor = numero;
+    }
+    else
+    {
+        if (numero > mayor)
+        {
+            mayor = numero;
+        }
+        if (numero < menor)
+        {
+            menor = numero;
+        }
+    }
+}
+while (contador < 10);
+
+Console.WriteLine($"La suma de todos los números ingresados es: {acumulador}");
+Console.WriteLine($"El número mayor ingresado es: {mayor}");
+Console.WriteLine($"El número menor ingresado es: {menor}");
+Console.WriteLine($"El promedio de todos los números ingresados es: {promedio}");
+Console.ReadKey();
+
+
+
+
+
